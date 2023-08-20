59b5f13f0dac29a7aa71fd9f5327cae7cf654a17
ez7930 <141891182+ez7930@users.noreply.github.com>
2023-08-19 21:43:15 -0500

    Add files via upload

---
+++ search.js
@@ -0,0 +1,15 @@
+
+function search_recipe() {
+    let input = document.getElementById('searchbar').value;
+    input=input.toLowerCase();
+    let recipe = document.getElementsByClassName('recipe');
+      
+    for (i = 0; i < recipe.length; i++) { 
+        if (!recipe[i].getElementsByTagName('h1')[0].innerHTML.toLowerCase().includes(input)) {
+            recipe[i].style.display="none";
+        }
+        else {
+            recipe[i].style.display="list-item";                 
+        }
+    }
+}
